3a56d4516943de799e3782ab9622501443f4472c
nwadike ebere favour <favsamlol@gmail.com>
2025-06-20 15:19:47 +0100

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,15 +3,23 @@ import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
 import {initDB} from "./config/db.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV==="production") job.start();
+
 app.use(rateLimiter);
 app.use(express.json());
 
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req,res) =>{
+    res.status(200).json({status: "ok"});
+});
 
 
 app.use("/api/transactions",transactionsRoute)
