266fcba298985c741785f0a2eb461dde4edb1841
nwadike ebere favour <favsamlol@gmail.com>
2025-06-21 13:40:44 +0100

    Final: added root and health routes with rateLimiter

--- src/server.js
+++ src/server.js
@@ -1,71 +1,43 @@
-// import express from "express";
-// import dotenv from "dotenv"
-// import rateLimiter from "./middleware/rateLimiter.js";
-// import transactionsRoute from "./routes/transactionsRoute.js";
-// import {initDB} from "./config/db.js";
-// import job from "./config/cron.js";
-//
-// dotenv.config();
-//
-// const app = express();
-//
-// if (process.env.NODE_ENV==="production") job.start();
-//
-// app.use(rateLimiter);
-// app.use(express.json());
-//
-//
-// const PORT = process.env.PORT || 5001;
-//
-// app.get("/api/health", (req,res) =>{
-//     res.status(200).json({status: "ok"});
-// });
-//
-//
-//
-// app.use("/api/transactions",transactionsRoute)
-//
-//
-// initDB().then(() => {
-//     app.listen(PORT, () => {
-//         console.log("Server is up and running on PORT:", PORT);
-//     });
-// })
-//
-//
-
-
 import express from "express";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
-import { initDB } from "./config/db.js";
+import {initDB} from "./config/db.js";
 import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") job.start();
+if (process.env.NODE_ENV==="production") job.start();
 
 app.use(rateLimiter);
 app.use(express.json());
+
+
+const PORT = process.env.PORT || 5001;
 
 
 app.get("/", (req, res) => {
     res.send("API is running 🚀");
 });
 
-app.get("/api/health", (req, res) => {
-    res.status(200).json({ status: "ok" });
+
+app.get("/api/health", (req,res) =>{
+    res.status(200).json({status: "ok"});
 });
 
-app.use("/api/transactions", transactionsRoute);
 
-const PORT = process.env.PORT || 5001;
+
+app.use("/api/transactions",transactionsRoute)
+
 
 initDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server is up and running on PORT:", PORT);
     });
-});
+})
+
+
+
+
